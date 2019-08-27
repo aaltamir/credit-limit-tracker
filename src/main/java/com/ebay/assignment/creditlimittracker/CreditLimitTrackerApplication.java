ad6d845267158aaa05b51d94195039b4f7b3a228
Ariel Altamirano <ariel.h.altamirano@gmail.com>
2019-08-27 23:23:05 +0200

    Setup Spring boot application.

---
+++ src/main/java/com/ebay/assignment/creditlimittracker/CreditLimitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ebay.assignment.creditlimittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditLimitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditLimitTrackerApplication.class, args);
+	}
+
+}
